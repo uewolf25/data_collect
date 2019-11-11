234017461cde039f4c191bb18e1dda70a009f4ab
wolf <nyan643.ym25mm10@gmail.com>
2019-11-11 19:24:25 +0900

    [complete]: read and count noun of 3 genre files

--- ranking.py
+++ ranking.py
@@ -4,10 +4,12 @@
 import os
 import os.path
 import bs4 
-import re
 import MeCab
 from collections import Counter
+import data_collect as dc
 
+categories = dc.categories
+text_files = dc.text_files
 
 ###############################################
 ### HTML文書を目的の本文の部分のみスクレイピングし、###
@@ -15,49 +17,64 @@ from collections import Counter
 ###############################################
 def html_parser():
 
-  file_path = []
+  for dir_name, text_list in zip(categories, text_files):
+    # 各ファイルのパスを格納する
+    file_path = []
+    all_files = os.listdir(dir_name)
 
-  all_files = os.listdir('file')
-  for local_file in all_files:
-    file_path.append(os.path.join("file", local_file))
-    
-  for local_file in file_path:
-    try:
-      with open(local_file) as f:
-        text = bs4.BeautifulSoup(f, "html.parser").select('p')
-    except IOError:
-      print('cannot be opened .')
-    except UnicodeDecodeError:
-      print(local_file + ' is cannot be decoded')
+    for local_file in all_files:
+      file_path.append(os.path.join(dir_name, local_file))
+      
+    for local_file in file_path:
+      # print(dir_name + ' : ' + text)
+      try:
+        with open(local_file) as f:
+          # pタグの部分だけ抜いてくる
+          text = bs4.BeautifulSoup(f, "html.parser").select('p')
+      except IOError:
+        print('cannot be opened .')
+      except UnicodeDecodeError:
+        print(local_file + ' is cannot be decoded')
 
-    for item in text:
-      article = ''
-      get_articles = str( item.get_text() )
-      article += get_articles
-      add_text(article)
+      for item in text:
+        article = ''
+        # タグ付きの文章のタグを除去し、文字列型に変換
+        get_articles = str( item.get_text() )
+        article += get_articles
+        add_text(article, text_list)
+    # 分析する
+    annalys_documents(text_list)
 
 #####################################
 ### 取得した文書をテキストに追記していく ###
 #####################################
-def add_text(str):
-  with open("text.txt", "a") as f:
+def add_text(str, text_file):
+  try:  
+    f = open(text_file, 'a')
+  except IOError:
+    print('cannot be opened .')
+  else:
     f.write(str)
 
 #########################################################################
 ### Mecabを用いて、取得した文書を解析する。                                  ###
 ### 表層形  品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音 ###
 #########################################################################
-def annalys_documents():
+def annalys_documents(file_text):
   # 名詞収集
   noun = ''
   # ファイルから読み込んだ文字列を格納
   text = ''
   count_noun = Counter()
   try:
-    with open("text.txt", "r") as f:
-      text = f.read()
+    f = open(file_text, 'r')
   except IOError:
     print('cannot be opend .')
+  else:
+    text = f.read()
+    f.close()
+  finally:
+    print('---------------------------------------')
 
   tagger = MeCab.Tagger()
   annalys_text = tagger.parse(text)
@@ -81,4 +98,3 @@ def annalys_documents():
 
 if __name__ == '__main__':
   html_parser()
-  annalys_documents()
