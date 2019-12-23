bf312dc0e14975ae73db4a0dcd75d1a9ec557066
wolf <nyan643.ym25mm10@gmail.com>
2019-12-23 19:53:47 +0900

    Update

--- src/key.py
+++ src/key.py
@@ -5,7 +5,7 @@ import os
 import os.path
 import dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(dotenv_path)
 
 #　環境変数をセット
